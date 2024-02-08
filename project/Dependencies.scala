046978e66ce5844b3c13d14379d3d309bb3161ed
Scala Steward <andreas@killaitis.de>
2024-02-08 01:48:40 +0000

    Update scalatest to 3.2.18

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ object Dependencies {
   val CatsEffectVersion        = "3.5.3"
   val CatsEffectTestingVersion = "1.5.0"
 
-  val ScalaTestVersion = "3.2.17"
+  val ScalaTestVersion = "3.2.18"
   val PostgresVersion  = "42.7.0"
   val LogbackVersion   = "1.4.14"
   val Log4CatsVersion  = "2.6.0"
