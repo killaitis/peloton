611a5542e05d3c9be11ccbb19e12aec7964054c4
Scala Steward <andreas@killaitis.de>
2025-02-11 02:18:04 +0000

    Update mysql-connector-j to 9.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
   val RSFlowAdaptersVersion           = "1.0.2"
 
   val PostgresVersion                 = "42.7.5"
-  val MySQLVersion                    = "9.1.0"
+  val MySQLVersion                    = "9.2.0"
   val CassandraJavaDriverVersion      = "4.17.0"
 
   val TestContainersVersion           = "1.18.3"
