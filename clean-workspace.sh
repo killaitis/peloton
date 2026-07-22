4471dc0da64c28c6b16905eae1d5ace67440ea8b
Andreas Killaitis <andreas@killaitis.de>
2026-07-22 13:00:23 +0200

    Added helper script to clean local environment

---
+++ clean-workspace.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Script to cleanup all tempoary files and directories created by sbt, bloop and Metals. Use this script 
+# if VS Code behaves "weird" or seems broken. Close VS Code, run this script and start VS Code again. 
+# Then re-import the project.
+
+for dir in .bloop .metals build target metals.sbt out
+do
+    find . -name $dir -exec rm -rf {} +
+done
